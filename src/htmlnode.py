8c5eb08d8686b324feba4c3db9f8becb8e2321db
prateek khened <code.prateekkhened@gmail.com>
2025-09-20 11:31:47 +0530

    leafnode implementation of the html whole node with testcases

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -1,5 +1,5 @@
 class HTMLNode:
-    def __init__(self, tag = None, value = None, children = None, props = None):
+    def __init__(self, tag=None, value=None, children=None, props=None):
         self.tag = tag
         self.value = value
         self.children = children
@@ -19,3 +19,16 @@ class HTMLNode:
     def __repr__(self):
         print(f"HTMLNode({self.tag}, {self.value}, {self.children}, {self.props})")
     
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag, value, None, props)
+    
+    def to_html(self):
+        if self.value is None:
+            raise ValueError("value field is empty")
+        if self.tag is None:
+            return self.value
+        return f'<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>'
+
+    def __repr__(self):
+        return f"LeafNode({self.tag}, {self.value}, {self.props})"
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,5 +1,5 @@
 import unittest
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 class TestHTMLNode(unittest.TestCase):
     def test_props_to_html_eq(self):
@@ -21,7 +21,24 @@ class TestHTMLNode(unittest.TestCase):
         node = HTMLNode("p", "india is my country")
         res = ''
         self.assertEqual(node.props_to_html(), res)
+
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), '<p>Hello, world!</p>')
     
+    def test_leaf_to_html_props_eq(self):
+        node = LeafNode("a", "Click me!", {"href": "https://www.google.com"})
+        res = '<a href="https://www.google.com">Click me!</a>'
+        self.assertEqual(node.to_html(), res)
+    
+    def test_leaf_to_html_not_eq(self):
+        node = LeafNode("p", "india is my country and all indians are my brothers and sisters")
+        self.assertNotEqual(node.to_html(), '<p>USA is my country and all Americans are my brothers and sisters</p>')
+    
+    def test_leaf_to_html_not_eq(self):
+        node = LeafNode("p", "india is my country and all indians are my brothers and sisters")
+        self.assertNotEqual(node.to_html(), '<h1>india is my country and all indians are my brothers and sisters</h1>')
+
 
 if __name__ == "__main__":
     unittest.main()
